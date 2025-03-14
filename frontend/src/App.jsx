6437a6669b3a7b94becfe76be550b5c1e6fbfb2b
Manmit Pal <manmitpal348@gmail.com>
2025-03-14 22:11:10 +0530

    Created jsx components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import {Routes, Route} from 'react-router-dom'
+import HomeScreen from './screen/HomeScreen'
+import SignUpScreen from './screen/SignUpScreen'
+import LoginScreen from './screen/LoginScreen'
+import SettingScreen from './screen/SettingScreen'
+import ProfileScreen from './screen/ProfileScreen'
 
 const App = () => {
   return (
     <div>
       
     <Navbar/>
-
+    <Routes>
+      <Route path='/' element={<HomeScreen/>} /> 
+      <Route path='/signup' element={<SignUpScreen/>} /> 
+      <Route path='/login' element={<LoginScreen/>} /> 
+      <Route path='/settings' element={<SettingScreen/>} /> 
+      <Route path='/profile' element={<ProfileScreen/>} /> 
+    </Routes>
     </div>
   )
 }
